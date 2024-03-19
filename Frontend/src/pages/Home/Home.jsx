035bb9f70ca2a06d5d624a22d77fbe8a9fb2070b
Your Name <you@example.com>
2024-03-19 15:54:00 +0600

    add frontend

--- Frontend/src/pages/Home/Home.jsx
+++ Frontend/src/pages/Home/Home.jsx
@@ -1,10 +1,14 @@
-// import React from 'react'
+/* eslint-disable no-unused-vars */
+
+ import React, { useEffect } from 'react'
 
 import Carousel from "./Carousel";
 
 // import image1 from "../../../assets/images/index";
 // import image2 from "../../../assets/images/index";
 // import image3 from "../../../assets/images/index";
+
+import axios from "axios"
 
 const items = [
   <div key={1} className="w-full h-64 bg-gray-300">
@@ -19,12 +23,25 @@ const items = [
   // Add more items as needed
 ];
 // const images = [{ path: image1 }, { path: image2 }, { path: image3 }];
+
 const Home = () => {
+
+  useEffect(() => {
+    
+    (async ()=> {
+     const response = await axios.get("https://e-commerce-mern-zqx9.onrender.com/test");
+     console.log(response.data)
+    })()
+  }, [])
+  
+
   return (
     // <div className="flex flex-col items-center justify-center h-screen">
     //   <h1>Infinity Carousel Example</h1>
     // </div>
     // <Carousel interval={5000} />
+
+
     <div>Home</div>
   );
 };
